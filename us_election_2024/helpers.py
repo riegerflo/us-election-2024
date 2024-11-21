5c325fddb4b682eaf380ccfbe5d024ca9a08230b
Florian Rieger <mail@riegerflo.com>
2024-11-21 23:59:03 +0100

    Calculate stat for one county

--- us_election_2024/helpers.py
+++ us_election_2024/helpers.py
@@ -10,3 +10,13 @@ def unique(df, column: str, sub_str: str):
     "Finds unique items in given column."
     return [item for item in df[column].unique() if sub_str in item]
     
+def filter_for(df, column, value):
+    return df[df[column]==value]
+
+
+def total_votes_per_precinct(df):
+    return df.groupby("Precinct")[["Early Voting", "Total Votes"]].sum()
+
+
+def filter_isin(df, column, values):
+    return df[df[column].isin(values)]
