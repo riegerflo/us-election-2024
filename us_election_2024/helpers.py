6e7f1a927595d449dd468462e4e2a302191d8599
Florian Rieger <mail@riegerflo.com>
2024-11-27 23:50:04 +0100

    Update load data

--- us_election_2024/helpers.py
+++ us_election_2024/helpers.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 NC_FILE_PATH = "../data/results_pct_20241105.txt"
 
-def read_data():
-    df = pd.read_csv(NC_FILE_PATH, delimiter="\t")
+def read_data(path=NC_FILE_PATH):
+    df = pd.read_csv(path, delimiter="\t")
     return df
 
 def unique(df, column: str, sub_str: str):
@@ -14,9 +14,31 @@ def filter_for(df, column, value):
     return df[df[column]==value]
 
 
+def contains(df, column, substr):
+    return df[df[column].str.contains(substr)]
+
+
 def total_votes_per_precinct(df):
     return df.groupby("Precinct")[["Early Voting", "Total Votes"]].sum()
 
 
 def filter_isin(df, column, values):
     return df[df[column].isin(values)]
+
+
+def filter_multiindex_by_tuples(df, filter_tuples):
+    """
+    Filter a pandas DataFrame with a MultiIndex based on a list of index tuples.
+    
+    Parameters:
+    - df: The pandas DataFrame with a MultiIndex.
+    - filter_tuples: A list of tuples, where each tuple corresponds to a specific 
+                      combination of index values to keep.
+                    
+    Returns:
+    - A filtered DataFrame.
+    """
+    # Create a list of tuples to match in the MultiIndex
+    filtered_df = df[df.index.isin(filter_tuples)]
+    
+    return filtered_df
